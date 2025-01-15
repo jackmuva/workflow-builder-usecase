8ca9430a39ff52a3b368f7bdae51578ff7330b2e
Jack Mu <jackmu@umich.edu>
2025-01-15 10:56:39 -0800

    header UI change

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<ChildProps> = (props) => {
     return (
         <div className={props.email ? "bg-stone-100 absolute top-0 left-0 w-screen z-20 py-2 md:px-40 items-center justify-between font-['Helvetica'] text-sm flex flex-row-reverse" :
             "absolute top-0 left-0 w-screen z-20 py-8 md:px-40 items-center justify-between font-['Helvetica'] text-sm flex flex-row-reverse"}>
-            {!props.email &&
+            {!props.email && <>
                 <a
                     href="https://www.useparagon.com/"
                     target="_blank"
@@ -29,9 +29,6 @@ const Header: React.FC<ChildProps> = (props) => {
                     />
                     <span>Built with Paragon</span>
                 </a>
-            }
-
-            {!props.email &&
                 <button onClick={props.toggle}
                     className={"flex justify-center " +
                         "dark:border-neutral-800 " +
@@ -41,8 +38,9 @@ const Header: React.FC<ChildProps> = (props) => {
                     <div className={props.toggleDown ? "text-amber-800" : ""}>
                         {props.toggleDown ? "Close Login Panel" : "Login to Demo"}
                     </div>
-                </button>}
-            {props.email &&
+                </button>
+            </>}
+            {props.email && <>
                 <div className={"flex flex-col space-y-1"}>
                     <div className={"font-semibold"}> Welcome {props.email.split("@")[0]}</div>
                     <button className={"border-2 border-gray-300 rounded-xl p-1 items-center justify-center " +
@@ -50,7 +48,24 @@ const Header: React.FC<ChildProps> = (props) => {
                         onClick={() => signOut()}>
                         Sign Out
                     </button>
-                </div>}
+                </div>
+                <a
+                    href="https://www.useparagon.com/"
+                    target="_blank"
+                    className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
+                >
+                    <Image
+                        className="rounded-xl"
+                        src="/paragon-logo.png"
+                        alt="Paragon Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <span>FlowKit Demo</span>
+                </a>
+
+            </>}
         </div>
     );
 }
