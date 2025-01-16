fe8de6ab6e21d5a1e8b8f00284d6ac0a38518116
Jack Mu <jackmu@umich.edu>
2025-01-16 08:04:02 -0800

    using an api request instead of the get integration metadata method

--- app/components/ui/reactflow/reactflow-sidebar.tsx
+++ app/components/ui/reactflow/reactflow-sidebar.tsx
@@ -22,8 +22,11 @@ export const ReactflowSidebar = ({ nodes, edges, newId }: { nodes: Array<any>, e
 	}
 
 	useEffect(() => {
+		console.log(paragonUser);
 		fetchActions().then((actions) => {
-			setSidebarState((prev) => ({ ...prev, integrations: paragon.getIntegrationMetadata(), actions: actions }));
+			fetchIntegrationMetadata().then((metadata: any) => {
+				setSidebarState((prev) => ({ ...prev, integrations: metadata, actions: actions }));
+			})
 		});
 	}, []);
 
@@ -59,6 +62,20 @@ export const ReactflowSidebar = ({ nodes, edges, newId }: { nodes: Array<any>, e
 		});
 		const body = await response.json();
 		console.log(body);
+	}
+
+	const fetchIntegrationMetadata = async () => {
+		const metadataUrl = "https://api.useparagon.com/projects/" + process.env.NEXT_PUBLIC_PARAGON_PROJECT_ID + "/sdk/metadata";
+
+		const headers = new Headers();
+		headers.append("Content-Type", "application/json");
+		headers.append("Authorization", "Bearer " + sessionStorage.getItem("jwt"));
+		const metadataResponse = await fetch(metadataUrl, {
+			method: "GET",
+			headers: headers
+		});
+		const metadataBody = await metadataResponse.json();
+		return metadataBody;
 	}
 
 	const fetchActions = async () => {
@@ -112,8 +129,6 @@ export const ReactflowSidebar = ({ nodes, edges, newId }: { nodes: Array<any>, e
 		setSidebarState((prev) => ({ ...prev, output: selectedNode.data.funcProperties.output }));
 	}
 
-
-
 	return (
 		<div className='basis-1/5 m-1 p-2 flex flex-col items-center space-y-1 h-[800px] overflow-y-auto border-l-2'>
 			<div className="flex justify-between space-x-4">
@@ -133,55 +148,60 @@ export const ReactflowSidebar = ({ nodes, edges, newId }: { nodes: Array<any>, e
 						<button className='flex space-x-2 p-2 items-center justify-between w-full border-b-2 border-b-stone-200'
 							onClick={integrationEnabled ? () => toggleDropdown(integration.type) : () => paragon.connect(integration.type, {})}>
 							<img src={integration.icon} style={{ maxWidth: "30px" }} />
-							<p>{integration.name}</p>
+							<p className={integrationEnabled ? "font-semibold" : "text-stone-400"}>
+								{integration.name}
+							</p>
 							{sidebarState.activeDropdown !== integration.type && <svg fill="#000000" height="15px" width="15px" version="1.1" id="Layer_1" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"></path> </g></svg>
 							}
 							{sidebarState.activeDropdown === integration.type && <svg fill="#000000" height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393 C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"></path> </g></svg>
 							}
 						</button>
-						{integration.type === sidebarState.activeDropdown &&
+						{
+							integration.type === sidebarState.activeDropdown &&
 							<div className="flex flex-col space-y-2 mt-2">
 								{sidebarState.actions[integration.type]?.map((action: any) => {
 									return (
 										<ActionButton key={action.function.name} action={action} addNode={addNode} pic={integration.icon} />);
 								})}
-								{/*<button onClick={() => paragon.connect(integration.type, {})}
-									className="border-2 font-bold text-sm rounded-md bg-stone-100 place-self-center w-fit p-1">
-									Configure
-								</button>*/}
+								<button onClick={() => paragon.connect(integration.type, {})}
+									className="border-b-2 border-l-2 font-semibold hover:font-bold text-sm w-fit p-1">
+									~ Configure {integration.type}
+								</button>
 							</div>
 						}
 					</div>
 				);
 			})
 			}
-			{selectedNode !== undefined && <div className="w-full py-2 flex flex-col justify-center items-center space-y-2">
-				<button className="place-self-end" onClick={() => closeNodeMenu()}> x </button>
-				<div className='flex flex-col space-y-2 '>
-					{Object.keys(selectedNode?.data?.functionData?.function.parameters.properties).map((prop: string) => {
-						return (
-							<label key={prop}>{prop}:
-								<input value={selectedNode.data?.funcProperties?.parameters[prop]} onChange={onChange} id="text" name={prop} className="rounded-lg p-1 nodrag ml-2 border-2" />
-							</label>
-						)
-					})
-					}
-					<button className='bg-green-200 hover:bg-green-400 rounded-lg border-2 p-1' onClick={testStep}> Test Step </button>
-					{sidebarState.output !== "" &&
-						<div className="flex flex-col space-y-1 shadow-md p-1 border-1 bg-stone-50 absolute bottom-5 w-72 right-5 ">
-							<div className="flex justify-between">
-								<div className="font-semibold text-sm">Last Test Output:</div>
-								<button onClick={clearOutput}>x</button>
+			{
+				selectedNode !== undefined && <div className="w-full py-2 flex flex-col justify-center items-center space-y-2">
+					<button className="place-self-end" onClick={() => closeNodeMenu()}> x </button>
+					<div className='flex flex-col space-y-2 '>
+						{Object.keys(selectedNode?.data?.functionData?.function.parameters.properties).map((prop: string) => {
+							return (
+								<label key={prop}>{prop}:
+									<input value={selectedNode.data?.funcProperties?.parameters[prop]} onChange={onChange} id="text" name={prop} className="rounded-lg p-1 nodrag ml-2 border-2" />
+								</label>
+							)
+						})
+						}
+						<button className='bg-green-200 hover:bg-green-400 rounded-lg border-2 p-1' onClick={testStep}> Test Step </button>
+						{sidebarState.output !== "" &&
+							<div className="flex flex-col space-y-1 shadow-md p-1 border-1 bg-stone-50 absolute bottom-5 w-72 right-5 ">
+								<div className="flex justify-between">
+									<div className="font-semibold text-sm">Last Test Output:</div>
+									<button onClick={clearOutput}>x</button>
+								</div>
+								<textarea readOnly className='w-full p-1 whitespace-pre-line z-30 h-96 overflow-scroll'
+									value={sidebarState.output} />
 							</div>
-							<textarea readOnly className='w-full p-1 whitespace-pre-line z-30 h-96 overflow-scroll'
-								value={sidebarState.output} />
-						</div>
-					}
+						}
+
+					</div>
 
 				</div>
-
-			</div>}
+			}
 			<div className="text-sm text-stone-400 pt-6 text-center">Integration actions powered by Paragon</div>
-		</div>
+		</div >
 	);
 };
